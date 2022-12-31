03f3c0f6746eab19a21f2806a4e5356cb217c41b
JasonVonKrueger <rpoore@gmail.com>
2022-12-30 21:43:14 -0500

    update

---
+++ .history/public/resources/js/main_20221230213930.js
@@ -0,0 +1,161 @@
+const socket = io()
+const messages = []
+let current_page_index = 0
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    const __sidebar = document.getElementById('sidebar')
+
+    socket.on('REFRESH_MESSAGES', refreshMessages)
+    socket.on('BUTTON_PUSHED', handleRemBtnPush)
+    socket.emit('GET_MESSAGES')
+
+    getWeather()
+
+    __sidebar.focus()
+
+    // handle modal closing
+    document.querySelector('.modal-close').onclick = function() {
+        document.querySelector('.modal').style.display = 'none'
+    }
+
+    document.onkeydown = function(e) {
+        let a = getActiveSidebarButton()
+        
+        switch(e.key) {
+            case 'ArrowDown':
+                if (document.activeElement.id === 'sidebar') {
+                    if (a === 'nav_home') {
+                        triggerEvent(document.getElementById('nav_pictures'), 'click')
+                    }
+
+                    if (a === 'nav_pictures') {
+                        triggerEvent(document.getElementById('nav_games'), 'click')
+                    }   
+                }
+
+            case 'ArrowUp':
+                // do something;
+        }      
+    }
+
+    // __sidebar.onfocus = function() {
+    //     __sidebar.onkeydown = function(e) {
+    //         switch(e.key) {
+    //             case 'ArrowDown':
+    //                 let next = __sidebar.firstChild.id
+    //                 alert(next)
+    //                 //triggerEvent(document.getElementById(next), 'click')
+    //             case 'ArrowUp':
+    //                 // do something;
+    //         }
+    //     }
+    // }
+})
+
+function getActiveSidebarButton() {
+    let b = document.querySelector('#sidebar .active')
+    let c = b.id
+   // let p = document.getElementById('sidebar').id
+    //let c = p.children
+    return c
+}
+
+function show(sel_el) {
+    // clear all the sectiions first
+    Array.from(document.querySelectorAll("[id^='section_']")).forEach(function(element) {
+        element.classList.add('hidden')
+    })
+
+    // clear active nav selections
+    Array.from(document.querySelectorAll("[id^='nav_']")).forEach(function(element) {
+        element.classList.remove('active')
+    })
+
+    document.getElementById(sel_el.id).classList.add('active')
+    document.getElementById(sel_el.id).focus()
+    document.getElementById(sel_el.id.replace('nav_', 'section_')).classList.remove('hidden')
+    //alert(document.activeElement.id)
+}
+
+function handleRemBtnPush(data) {
+    //alert('yo' + data.button)
+    triggerEvent(document.getElementById('nav_games'), 'click')
+}
+
+function openModal(mo) {
+    document.getElementById(mo).style.display = 'block'
+}
+
+function triggerEvent(elem, event) {
+    let clickEvent = new Event(event)
+    elem.dispatchEvent(clickEvent)
+}
+
+function refreshMessages(data) {
+    const message_block = document.querySelector('#message_block')
+
+    for (let i = 0; i < data.length; i++) {
+        const con = document.createElement('div')
+        con.classList.add('msg-box')
+
+        const sender = document.createElement('div')
+        sender.classList.add('sender')
+        sender.innerHTML = `${data[i].from} <br /> ${data[i].date}`
+
+        const message = document.createElement('div')
+        message.classList.add('message')
+        message.innerHTML = data[i].message
+
+        con.appendChild(sender)
+        con.appendChild(message)
+
+        message_block.appendChild(con)
+    }
+}
+
+async function getWeather() {
+    const key = '8815275c285e40149bd222225222712'
+    const url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=30236&aqi=no`
+
+    let response = await fetch(url)
+    let data = await response.json()
+    const {
+        location,
+        current
+    } = data
+
+    const markup = ` 
+    <h2><span>${location.name}, ${location.region}</span></h2> 
+    <img src=${current.condition.icon} style="width: 100px">
+    <table style="width: 100%">
+    <tr>
+    <td></td>
+    <td style="font-size: 2em;">${Math.round(current.temp_f)}<sup style="font-size: .6em">°F</sup></td>
+    <td style="font-size: 2em;">${current.humidity}%</td>
+    </tr>
+    </table>`
+
+    weather_block.innerHTML = markup
+}
+
+async function getAlbums() {
+    const url = 'https://photoslibrary.googleapis.com/v1/albums'
+    const client_id = '948657149825-l9p5dkb7od1c85kupbutjqrfe1guakho.apps.googleusercontent.com'
+    const client_secret = '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'
+    const api_key = 'AIzaSyAzNeh8YHfwLXpsT92c1FMyopxhSLVvuUg'
+    // key=API_KEY
+
+    let response = await fetch(url)
+    let data = await response.json()
+}
+
+async function fetchContent(page) {
+
+    let response = await fetch(page)
+    let data = await response.text()
+    document.getElementById('bob').innerHTML = data
+    console.log('RGP: ' + data)
+    //return { stuff: data }
+    //let response = fetch(page)
+
+}
---
+++ .history/src/app_20221230211642.js
@@ -0,0 +1,70 @@
+/* Server for Boarderly */
+
+const http = require('http')
+const express = require('express')
+const socket_io = require('socket.io')
+const path = require('path')
+const messages = []
+let current_page_index = 0
+
+const app = express();
+app.use(express.static('public'))
+app.use(express.json())
+
+const server = http.Server(app)
+const io = socket_io(server)
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+
+	socket.on('GET_MESSAGES', function() {
+    io.emit('REFRESH_MESSAGES', messages)
+		//socket.broadcast.emit('REFRESH_MESSAGES', messages)
+	})
+
+	socket.on('REFRESH_MESSAGES', function() {
+		socket.broadcast.emit('REFRESH_MESSAGES', messages)
+	})
+
+	socket.on('BUTTON_PUSHED', function(data) {
+    console.log('button pushed ' + data.button)
+		socket.broadcast.emit('BUTTON_PUSHED', data)
+	})
+
+	socket.on('POST_MESSAGE', function(data) {
+    let msg = {
+      from: data.from,
+      message: data.message,
+      date: data.date
+    }
+  
+    messages.push(msg)
+
+		socket.broadcast.emit('REFRESH_MESSAGES', messages)
+	})
+
+});
+
+
+
+
+app.get('/getMessages', function(req, res) {
+  res.send(JSON.stringify(messages)) 
+})
+
+app.post('/sendmessage', function(req, res) {
+  let msg = {
+    from: req.body.from,
+    message: req.body.message
+  }
+
+  messages.push(msg)
+  //socket.broadcast.emit('REFRESH_MESSAGES', messages)
+  console.log(messages)
+})
+
+server.listen(9520, '0.0.0.0', () =>
+  console.log('Example app listening on port 9520!'),
+);
+
+
